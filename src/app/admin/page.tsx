6fad1ffebf0562489f43ee2fd415e30fb57786f6
KetanKudikyal <krotusppp@gmail.com>
2024-12-01 16:30:39 +0530

    feat: ✨ refactor transaction execution and fix wallet address handling

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -7,23 +7,20 @@ import { Transaction } from '@mysten/sui/transactions';
 const client = new SuiClient({ url: getFullnodeUrl('devnet') });
 
 const walletAddress = '0x3af3fd05d135413e03ddc942ee8e4042f4eb624e8858d3fe4e0e7e5f117b66b3'
+const treasuryCapObjectId = "ELPtL7R97taJi83m5MuNkbLuVdDCydku8SbjCwrnoh8B"; // Replace with the actual TreasuryCap object ID
 
 const AdminPage = () => {
     const tx = new Transaction();
 
-
-    const wallet = new TextEncoder().encode(walletAddress);
-
-
     const execute = async () => {
-        debugger
         const exampleMnemonic = 'bomb code roast cross trust proud size song render spirit travel fitness';
         const keypair = Ed25519Keypair.deriveKeypair(exampleMnemonic);
-        tx.setSender(keypair.getPublicKey().toSuiAddress());
+        const sender = keypair.getPublicKey().toSuiAddress()
+        tx.setSender(sender);
         await tx.sign({ client, signer: keypair });
         tx.moveCall({
             target: '0x7ae500436282efcc230f679b813f5f2f6c82f42bbf813cbc7ea63a97f2b67e85::loyalty::reward_user',
-            arguments: [tx.pure(wallet), tx.pure(1)],
+            arguments: [tx.object("0xdcdc410672bf32087afcfe8ceaf2309ee7eaac5d0ce98a5a22dbdc9d189a66a9"), tx.pure.u64(2), tx.pure.address(walletAddress)],
         });
         const result = await client.signAndExecuteTransaction({
             transaction: tx,
